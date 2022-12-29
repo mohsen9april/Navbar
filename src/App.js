e8613eec60a81ef99642a431b6f254446073dcae
Mohsen9april <Mohsen9april@gmail.com>
2022-12-29 18:03:08 +0330

    made structure

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar_container"> im side bar</div>
+      <div className="body_container">
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default App;
