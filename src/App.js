a43af59417ff0d1d9b900114aa82de56344c2c20
Mohsen9april <Mohsen9april@gmail.com>
2022-12-30 14:46:42 +0330

    Add motion to Items and Profile

--- src/App.js
+++ src/App.js
@@ -9,12 +9,68 @@ import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
 import ColorLensRoundedIcon from "@mui/icons-material/ColorLensRounded";
 import SettingsRemoteRoundedIcon from "@mui/icons-material/SettingsRemoteRounded";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [open, setOpen] = useState(true);
+
+  const handleToggle = () => {
+    console.log("Clicked !");
+    setOpen(!open);
+  };
+
+  const sideContainerVariants = {
+    true: {
+      width: "15rem",
+    },
+    false: {
+      width: "6rem",
+      transition: {
+        delay: 0.5,
+      },
+    },
+  };
+
+  const sidebarVariants = {
+    true: {
+      width: "15rem",
+    },
+    false: {
+      width: "3rem",
+      transition: {
+        delay: 0.5,
+      },
+    },
+  };
+
+  const profileVariants = {
+    true: {
+      alignSelf: "center",
+      width: "4rem",
+    },
+    false: {
+      alignSelf: "flex-start",
+      marginTop: "2rem",
+      width: "3rem",
+    },
+  };
+
   return (
     <div className="App">
-      <div className="sidebar_container">
-        <div className="sidebar">
+      <motion.div
+        data-Open={open}
+        variants={sideContainerVariants}
+        initial={`${open}`}
+        animate={`${open}`}
+        className="sidebar_container"
+      >
+        {/* Sidebar */}
+        <motion.div
+          initial={`${open}`}
+          animate={`${open}`}
+          variants={sidebarVariants}
+          className="sidebar"
+        >
           {/* lines_icon */}
           <motion.div
             whileHover={{
@@ -29,30 +85,50 @@ function App() {
                 duration: 0.25,
               },
             }}
+            onClick={handleToggle}
             className="lines_icon"
           >
             <TocRoundedIcon />
           </motion.div>
 
           {/* profile */}
-          <div className="profile">
+          <motion.div
+            layout
+            initial={`${open}`}
+            animate={`${open}`}
+            variants={profileVariants}
+            transition={{ duration: 0.5 }}
+            whileHover={{
+              backgroundColor: "rgba(255, 255, 255, 0.3)",
+              boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+              backdropFilter: "blur(5.5px)",
+              WebkitBackdropFilter: "blur(5.5px)",
+              border: "1px solid rgba( 255, 255, 255, 0.18 )",
+              cursor: "pointer",
+            }}
+            className="profile"
+          >
             <img
               src="https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=1380&t=st=1672326453~exp=1672327053~hmac=5bfb589c4e6c4d1bb9d9126679ee77e7e5c1067b67b96f3e402a3ed1345d017b"
               alt="profile_img"
             />
-          </div>
+          </motion.div>
           {/* Groups */}
           <div className="groups">
             {/* Group 1  */}
             <div className="group">
-              <h3>ANALYTICS</h3>
+              <motion.h3 animate={{ opacity: open ? 1 : 0, height: open ? "auto" : 0 }}>
+                ANALYTICS
+              </motion.h3>
               <ItemSidebar icon={<DashboardRoundedIcon />} name="Dashbord" />
               <ItemSidebar icon={<BarChartRoundedIcon />} name="Performance" />
             </div>
 
             {/* Group 2  */}
             <div className="group">
-              <h3>CONTENT</h3>
+              <motion.h3 animate={{ opacity: open ? 1 : 0, height: open ? "auto" : 0 }}>
+                CONTENT
+              </motion.h3>
               <ItemSidebar icon={<AttachMoneyRoundedIcon />} name="Sales" />
               <ItemSidebar icon={<AssignmentTurnedInRoundedIcon />} name="Checklist" />
               <ItemSidebar icon={<AccountCircleRoundedIcon />} name="Costumers" />
@@ -60,15 +136,41 @@ function App() {
 
             {/* Group 3  */}
             <div className="group">
-              <h3>COSTOMIZATION</h3>
+              <motion.h3 animate={{ opacity: open ? 1 : 0, height: open ? "auto" : 0 }}>
+                COSTOMIZATION
+              </motion.h3>
               <ItemSidebar icon={<SettingsRemoteRoundedIcon />} name="Segments" />
               <ItemSidebar icon={<ColorLensRoundedIcon />} name="Themes" />
             </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
       <div className="body_container">
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
+        <h2>Body</h2>
+        <hr />
         <h2>Body</h2>
         <hr />
         <h2>Body</h2>
